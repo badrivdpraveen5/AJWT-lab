97819f9b4b9a1b8280a2b7098745f2382005a7df
Venkata Durga Praveen Badri <badrivdpraveen5@gmail.com>
2023-03-14 13:09:59 +0530

    first commit

--- WEB-INF/classes/Update5.java
+++ WEB-INF/classes/Update5.java
@@ -8,21 +8,21 @@ throws ServletException, IOException {
 response.setContentType("text/html");  
 PrintWriter pw = response.getWriter();            
 String rollno=request.getParameter("roll");  
+String adr=request.getParameter("ad");  
      Statement stmt = null;    
 try{
 Class.forName("oracle.jdbc.driver.OracleDriver");  
-Connection con=DriverManager.getConnection(
-"jdbc:oracle:thin:@localhost:1521:xe","system","manager");                
+Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");                
 stmt = con.createStatement();
-            String sql = "update table students set adr=? where rno=?";
+            String sql = "update  students set adr=? where rno=?";
             //count will give you how many records got updated
            //String sql = "update people set firstname=? ,lastname=? where id=?";
 PreparedStatement preparedStatement = null;
 preparedStatement  = con.prepareStatement(sql);
-preparedStatement.setString(5, "nrt");
-preparedStatement.addBatch();
-int[] affectedRecords = preparedStatement.executeBatch();
-
+preparedStatement.setString(1, adr);
+preparedStatement.setString(2, rollno);
+preparedStatement.executeUpdate();
+pw.println("updated");
 }
 catch (Exception e2) {e2.printStackTrace();}            
 finally{pw.close();}   
